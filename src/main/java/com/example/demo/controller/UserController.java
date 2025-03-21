90b99fe517ea53cd97cb61d50b6486ed0ea16086
Jeet <jeet.s@iqm.com>
2025-03-22 00:00:44 +0530

    IMPLEMENT: Add endpoints has preauthorize annotation.

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PreAuthorize("hasAnyAuthority('MERCHANT', 'ADMIN', 'EMPLOYEE', '')")
     @PostMapping("/register")
     public CommonResponse<User> register(@RequestBody SignUpRequestObject user) {
         LOGGER.debug("In UserController::register for email {}", user.getEmail());
@@ -41,6 +43,7 @@ public class UserController {
         return commonResponse;
     }
 
+    @PreAuthorize("hasAnyAuthority('MERCHANT', 'ADMIN', 'EMPLOYEE', '')")
     @PostMapping("/login")
     public CommonResponse<Map<String, Object>> login(HttpServletRequest httpServletRequest,
                                                      Principal principal,
@@ -51,6 +54,7 @@ public class UserController {
         return commonResponse;
     }
 
+    @PreAuthorize("hasAnyAuthority('MERCHANT', 'ADMIN', 'EMPLOYEE', '')")
     @PostMapping("/verify-otp")
     public CommonResponse<User> verifyOtp(@RequestBody VerifyOtpRequestObject verifyOtpRequestObject) {
         LOGGER.debug("In UserController::verifyOtp for userID {}", verifyOtpRequestObject.getUserId());
@@ -59,6 +63,7 @@ public class UserController {
         return commonResponse;
     }
 
+    @PreAuthorize("hasAnyAuthority('MERCHANT', 'ADMIN', 'EMPLOYEE', '')")
     @GetMapping("/{userId}/resend-otp")
     public CommonResponse<String> resendOtp(@PathVariable("userId") int userId){
         LOGGER.debug("In UserController::resendOtp for userID {}", userId);
@@ -67,6 +72,7 @@ public class UserController {
         return commonResponse;
     }
 
+    @PreAuthorize("hasAnyAuthority('MERCHANT', 'ADMIN', 'EMPLOYEE', '')")
     @PostMapping("/reset-password")
     public CommonResponse<User> resetPassword(@RequestBody ResetPassword resetPassword) {
         LOGGER.debug("In UserController::resetPassword for userID {}", resetPassword.getUserId());
